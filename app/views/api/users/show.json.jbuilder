5882d0a7bb37a19d15ec22a68a38b86560f3900d
JasmineKobata <jasmineorjasnice@gmail.com>
2023-02-28 15:20:53 -0800

    files refactored for render

---
+++ app/views/api/users/show.json.jbuilder
@@ -0,0 +1,3 @@
+json.user do
+    json.extract! @user, :id, :email, :firstname, :lastname, :created_at, :updated_at
+end
---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,73 @@
+import './LoginForm.css';
+import { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect, Link } from 'react-router-dom';
+import Navigation from '../Navigation'
+
+export default function LoginFormPage() {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+
+    if (sessionUser) return <Redirect to="/" />;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setErrors([]);
+        return dispatch(sessionActions.login({ email, password }))
+            .catch(async (res) => {
+                let data;
+                try {
+                    data = await res.clone().json();
+                } catch {
+                    data = await res.text();
+                }
+                if (data?.errors) setErrors(data.errors);
+                else if (data) setErrors([data]);
+                else setErrors([res.statusTest]);
+            });
+    }
+
+    return (
+        <>
+        <Navigation/>
+        <form onSubmit={handleSubmit}>
+            <div className="login">
+                <ul>{errors.map(error => <li key={error}>{error}</li>)}</ul>
+                <div className="innerblock">
+                    <h2>Welcome Back.<br/>Log in and start exploring.</h2>
+                    <label className="email">
+                        <input className="inputText"
+                            type="text"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                        <div className="prelabel" id='email'>
+                            <span className="floating-label">Email Address</span>
+                        </div>
+                    </label><br></br><br></br>
+                    <label className="password">
+                        <input className="inputText"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                        <div className="prelabel" id='password'>
+                            <span className="floating-label">Password</span>
+                        </div>
+                    </label><br></br><br></br>
+                    <button type="submit">Log In</button><br></br><br></br>
+                    <div className="link">
+                        Don't have an account? <Link to='/signup'>Sign up for free</Link>
+                    </div>
+                </div>
+            </div>
+        </form>
+        </>
+    );
+}
