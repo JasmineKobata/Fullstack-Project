832a48ebb236a8115d0d05bdf2cb2f58f3b30faa
JasmineKobata <jasmineorjasnice@gmail.com>
2023-03-09 15:11:12 -0800

    add destroy tables

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,5 +1,5 @@
 class Trail < ApplicationRecord
-    validates :park_id, :name, :description, :length, :elevation, :long, :lat, presence: true
+    validates :park_id, :name, :difficulty, :description, :length, :elevation, :long, :lat, presence: true
     belongs_to :park
     has_one_attached :image
     has_many_attached :images
