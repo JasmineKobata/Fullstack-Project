0d21f00ae5199759aba1451ca06d9c14f2f5ebbe
JasmineKobata <jasmineorjasnice@gmail.com>
2023-03-09 17:27:57 -0800

    render upload attempt

--- db/migrate/20230308050110_create_reviews.rb
+++ db/migrate/20230308050110_create_reviews.rb
@@ -3,9 +3,8 @@ class CreateReviews < ActiveRecord::Migration[7.0]
     create_table :reviews do |t|
       t.integer :rating
       t.text :body
-      t.text :type
       t.bigint :author_id
-      t.bigint :park_or_trail_id
+      t.bigint :trail_id
 
       t.timestamps
     end
