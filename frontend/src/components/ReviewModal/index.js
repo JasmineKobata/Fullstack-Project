7e0089cf9bf07e7954ac4340e2400287bb4b5270
JasmineKobata <jasmineorjasnice@gmail.com>
2024-02-13 16:05:00 -0800

    started work on fixing ratings hover

--- frontend/src/components/ReviewModal/index.js
+++ frontend/src/components/ReviewModal/index.js
@@ -53,6 +53,13 @@ export default function ReviewModal({trail, reviews}) {
             || Object.values(reviews).some(e => e.authorId === session.user.id);
     }, [reviews, session.user])
 
+    // useEffect(() => {
+    //     const stars = document.getElementsByClassName("stars");
+    //     for (let i=0; i < stars.length; i++) {
+    //         stars[i].addEventListener("mouseover", (e) => console.log("HI"))
+    //     }
+    // }, [reviews, session.user])
+
     return (
         <>
             <button className='reviewButton' onClick={modalHandler}>{reviewButtonText}</button>
@@ -62,23 +69,28 @@ export default function ReviewModal({trail, reviews}) {
                     <p className='modalName'>{trail.name}</p>
                     <p className="modalRating">Rating</p>
                     <input className="stars" type="radio" id="1" value="1"
-                        onChange={(e) => setRating(e.target.value)}
+                        onClick={(e) => setRating(e.target.value)}
+                        // onMouseOver={(e) => setRating(e.target.value)}
                         checked={rating>=1 ? "checked" : ""}></input>
                         <label htmlFor='1'>&#9733;&nbsp;</label>
                     <input className="stars" type="radio" id="2" value="2"
-                        onChange={(e) => setRating(e.target.value)}
+                        onClick={(e) => setRating(e.target.value)}
+                        // onMouseOver={(e) => setRating(e.target.value)}
                         checked={rating>=2 ? "checked" : ""}></input>
                         <label htmlFor='2'>&#9733;&nbsp;</label>
                     <input className="stars" type="radio" id="3" value="3"
-                        onChange={(e) => setRating(e.target.value)}
+                        onClick={(e) => setRating(e.target.value)}
+                        // onMouseOver={(e) => setRating(e.target.value)}
                         checked={rating>=3 ? "checked" : ""}></input>
                         <label htmlFor='3'>&#9733;&nbsp;</label>
                     <input className="stars" type="radio" id="4" value="4"
-                        onChange={(e) => setRating(e.target.value)}
+                        onClick={(e) => setRating(e.target.value)}
+                        // onMouseOver={(e) => setRating(e.target.value)}
                         checked={rating>=4 ? "checked" : ""}></input>
                         <label htmlFor='4'>&#9733;&nbsp;</label>
                     <input className="stars" type="radio" id="5" value="5"
-                        onChange={(e) => setRating(e.target.value)}
+                        onClick={(e) => setRating(e.target.value)}
+                        // onMouseOver={(e) => setRating(e.target.value)}
                         checked={rating>=5 ? "checked" : ""}></input>
                         <label htmlFor='5'>&#9733;</label>
                     <br></br><p className='modalBody'>Review</p>
